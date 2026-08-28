a05df72eaa65341c3ec67b054d13fd8545b20bfe
sawerat70-code <sawerat70@gmail.com>
2026-08-28 15:59:55 +0500

    modified the names for users

--- server/models/Message.js
+++ server/models/Message.js
@@ -6,8 +6,8 @@ const mongoose = require("mongoose");
 
 const messageSchema = new mongoose.Schema(
   {
-    from: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-    to: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+    sender: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+    receiver: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
     text: { type: String, required: true, trim: true, maxlength: 2000 },
     read: {type:Boolean,default:false,},
   },
